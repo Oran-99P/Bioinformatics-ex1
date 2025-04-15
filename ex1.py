0a5cb6859b40ae32a5b89dd088b8633914d6ac4b
Oran-99P <p.o.3oran@gmail.com>
2025-04-15 15:11:31 +0300

    Q2

--- ex1.py
+++ ex1.py
@@ -207,6 +207,7 @@ def search_for_glider_patterns_3x3(max_generations=30, wraparound=True):
     runs automaton, and checks if any of them becomes a glider.
     """
     N = 10
+    MAX_TRIES = 512 #for similarity with 4x4
     tries = 0
 
     print(f"Scanning 3x3 patterns (wraparound = {'on' if wraparound else 'off'})...")
